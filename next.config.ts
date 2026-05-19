4589275c3fcce78be9411653af853293efbee675
Khalid Rinaldy <alid.rinaldy@gmail.com>
2026-05-19 19:47:48 +0700

    config image protocol

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,27 @@
 import type { NextConfig } from "next";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const nextConfig: NextConfig = {
   /* config options here */
+  reactStrictMode: false,
+  output: "standalone",
+  outputFileTracingRoot: __dirname,
   images: {
     formats: ["image/avif", "image/webp"],
-    unoptimized: true,
-  }
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+      {
+        protocol: "http",
+        hostname: "**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
